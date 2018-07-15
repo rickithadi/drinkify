4733b20f521e9cbfdfd215f489f312818575de53
rickithadi <rickithadi@gmail.com>
2018-07-15 16:34:38 +0800

    pubg bb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ActivatedRoute, RouterModule, CanActivate } from "@angular/router";
+import { Routes } from "@angular/router";
 
+import { HttpModule } from "@angular/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { StuffComponent } from './stuff/stuff.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "contact", component: ContactComponent },
+    { path: "stuff", component: StuffComponent }
+    ,
 
+    { path: "blog", component: BlogComponent }
+    ,
+    { path: "**", component: HomeComponent }
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        FooterComponent,
+        StuffComponent,
+        BlogComponent,
+        ContactComponent
+    ],
+    imports: [
+        AngularFontAwesomeModule,
+        HttpModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FormsModule,
+        NgbModule.forRoot(),
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
