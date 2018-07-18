1a7327145ded777a4d85718f9d7afb7d0b9bcf89
rickithadi <rickithadi@gmail.com>
2018-07-19 00:55:41 +0800

    forgot about ie

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,8 @@ import { Router } from '@angular/router';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+    user: string;
+    pass: string;
     constructor(private admin: AdminServiceService, private route: Router) { }
 
     ngOnInit() {
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+    private toggleButton: any;
+    private sidebarVisible: boolean;
+    isNavbarCollapsed = true;
+    collapse: boolean = true;
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
