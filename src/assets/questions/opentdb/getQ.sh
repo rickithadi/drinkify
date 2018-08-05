84d7269be4bf5b9c8bc75ffb6e4a61f97c5f09a9
rickithadi <rickithadi@gmail.com>
2018-08-05 15:01:59 +0800

    bash boyz

---
+++ src/assets/questions/opentdb/getQ.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+curl "https://opentdb.com/api.php?amount=20&category=9" >  c9.json
+curl "https://opentdb.com/api.php?amount=20&category=10" > c10.json
+curl "https://opentdb.com/api.php?amount=20&category=11" > c11.json
+curl "https://opentdb.com/api.php?amount=20&category=12" > c12.json
+curl "https://opentdb.com/api.php?amount=20&category=13" > c13.json
+curl "https://opentdb.com/api.php?amount=20&category=14" > c14.json
+curl "https://opentdb.com/api.php?amount=20&category=15" > c15.json
+curl "https://opentdb.com/api.php?amount=20&category=16" > c16.json
+curl "https://opentdb.com/api.php?amount=20&category=17" > c17.json
+curl "https://opentdb.com/api.php?amount=20&category=18" > c18.json
+curl "https://opentdb.com/api.php?amount=20&category=19" > c19.json 
+curl "https://opentdb.com/api.php?amount=20&category=20" > c20.json
+curl "https://opentdb.com/api.php?amount=20&category=21" > c21.json
+curl "https://opentdb.com/api.php?amount=20&category=22" > c22.json
+curl "https://opentdb.com/api.php?amount=20&category=23" > c23.json
+curl "https://opentdb.com/api.php?amount=20&category=24" > c24.json
+curl "https://opentdb.com/api.php?amount=20&category=25" > c25.json
+curl "https://opentdb.com/api.php?amount=20&category=26" > c26.json
+curl "https://opentdb.com/api.php?amount=20&category=27" > c27.json
+curl "https://opentdb.com/api.php?amount=20&category=28" > c28.json
+curl "https://opentdb.com/api.php?amount=20&category=29" > c29.json
+curl "https://opentdb.com/api.php?amount=20&category=30" > c30.json
+curl "https://opentdb.com/api.php?amount=20&category=31" > c31.json
+curl "https://opentdb.com/api.php?amount=20&category=32" > c32.json
+                                                                  
